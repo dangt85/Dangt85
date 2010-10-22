fa653afa9b13a30e0b6be4e8887b9bc6cdc56291
Daniel Gonzalez <dangt85@gmail.com>
2010-10-22 15:58:37 -0430

    first commit creating new GAE project for personal portfolio

---
+++ src/com/appspot/dangt85/Dangt85Servlet.java
@@ -0,0 +1,13 @@
+package com.appspot.dangt85;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Dangt85Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
